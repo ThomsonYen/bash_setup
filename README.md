0c506f736ee4ca72af61efaefd40072519c42c6a
Tzu-Ching Yen <tyen@login16.chn.perlmutter.nersc.gov>
2025-10-08 08:51:43 -0700

    Add uv setup instructions

---
+++ README.md
@@ -0,0 +1,21 @@
+# How to setup compute
+
+
+## Create ssh profile
+
+For example:
+```
+Host DataCrunch01
+  HostName 65.109.75.32
+  User root
+  IdentityFile <ssh_file_path>
+```
+
+## Git Clone this Repo
+
+1. Setup zshrc: run `zshrc/_main_setup.sh`
+2. Setup uv
+  - run `uv/_main_setup.sh`
+  - edit uv paths at the top of `~/.shell_setup/uv_setup.sh`
+
+## [Optional] Create cheatsheet.sh w/ compute-specific commands
---
+++ uv/_main_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download uv if not already installed
+if ! command -v uv &> /dev/null; then
+    echo "uv could not be found, installing..."
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+    echo "uv is already installed."
+fi
+
+# Change executation location to script's directory
+cd "$(dirname "$0")" || exit 1
+
+mkdir ~/.shell_setup
+
+cp uv_setup.sh ~/.shell_setup/uv_setup.sh
+echo "source ~/.shell_setup/uv_setup.sh" >> ~/.zshrc
